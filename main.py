84d53413eba53060c15c684f1641f67e4932fa09
vini waki <viniwte@gmail.com>
2020-09-21 02:26:11 -0300

    improved

--- main.py
+++ main.py
@@ -1,66 +1,127 @@
 # pip install selenium
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import time
+
+player = 'ytd-player#ytd-player'
+skip_countdown = 'span.countdown-next-to-thumbnail'
+skip_container = 'span.ytp-ad-skip-button-container'
+skip_button = 'div.ytp-ad-skip-button-text'
+video_duration = 'span.ytp-time-duration'
+autoplay_toggle = 'div#toggleButton'
+autoplay_active = 'paper-toggle-button[aria-pressed="true"]'
+search_bar = 'input#search'
+search_button = 'button#search-icon-legacy'
+video_title = '#container > h1'
+views_count = '#info-contents div#count'
+other_videos = 'span#video-title'
+
+def element(locator):
+    try:
+        return driver.find_element_by_css_selector(locator)
+    except:
+        return
+
+def elements(locator):
+    try:
+        return driver.find_elements_by_css_selector(locator)
+    except:
+        return
+
+def has_element(locator):
+    try:
+        driver.implicitly_wait(2)
+        return element(locator)
+    except:
+        driver.implicitly_wait(10)
+        return False
+    finally:
+        driver.implicitly_wait(10)
+
+def has_ad():
+    return has_element('div[id*=simple-ad-badge]')
+
+def handle_ads():
+    if (has_ad()):
+        if (has_element(skip_container)):
+            element(skip_button).click()
+        else:
+            time.sleep(getDuration())
+        time.sleep(1)
+        handle_ads()
+
+def to_sec(time):
+    aux = time.split(':')
+    minutes = int(aux[0])*60
+    seconds = int(aux[1])
+    return minutes + seconds
+
+def getDuration():
+    ret = driver.find_element_by_css_selector(video_duration).get_attribute('innerHTML')
+    if (':' in ret):
+        return to_sec(ret)
+
+def disable_autoplay():
+    if (has_element(autoplay_active)):
+        element(autoplay_toggle).click()
 
 def welcome_text():
     print('Hi, thanks for using my program :)\n')
     print('=====================================================\n')
 
-def ask_login():
-    pwd = ''
-    email = input('Enter your e-mail or just press enter if you dont want to log in\n')
-    if (email != ''):
-        pwd = input('Enter the password (I swear im not stealing data)\n')
-    return (email, pwd)
-
-def login(info):
-    if (info[0] != ''):
-        driver.find_elements_by_css_selector('paper-button[aria-label*="Sign in"]')[0].click()
-        driver.find_element_by_css_selector('input[type=email]').send_keys(info[0])
-        driver.find_elements_by_css_selector('#identifierNext > div > button > div')[1].click()
-        driver.find_element_by_css_selector('input[type=password]').send_keys(info[1])
-        driver.find_elements_by_css_selector('#passwordNext > div > button > div')[1].click()
-        time.sleep(2)
-        try:
-            driver.find_element_by_css_selector('#identity-prompt-confirm-button > span').click()
-            time.sleep(4)
-        finally:
-            # nothing lol
-            print('uhmmmm')
-
 def ask_for_vid():
-    # get the video you wanna stream
     vid = input('What video do you want to stream? (be specific)\n')
-    if vid == '' : vid = 'you and i dreamcatcher'
+    if vid == '' :
+        print('Nothing was typed...\n')
+        return ask_for_vid()
     return vid
 
-def watch_main_vid(vid):
-    search_bar = driver.find_element_by_css_selector('input#search')
-    search_bar.send_keys(vid)
-    search_but = driver.find_element_by_css_selector('button#search-icon-legacy')
-    search_but.click()
-    # bot clicks the 1st result
-    thumbnail = driver.find_elements_by_css_selector('a#video-title')[0]
-    thumbnail.click()
-    time.sleep(2)
-    log_mv_info()
-    watch_video()
+def goto_vid(vid):
+    element(search_bar).clear()
+    element(search_bar).send_keys(vid)
+    element(search_button).click()
+    time.sleep(3)
+    elements('a#video-title')[0].click()
 
-def log_mv_info():
+def getTitle():
+    video_title_text = element(video_title).text
+    if (video_title_text == ''):
+        return getTitle()
+    return video_title_text
+
+def getViews():
+    views = element(views_count).text
+    if (views == ''):
+        return getViews()
+    return views
+
+def log_info():
+    time.sleep(1)
     print ('[' + time.strftime("%b %d %H:%M:%S", time.gmtime()) + ']')
-    video_title = driver.find_element_by_css_selector('#container > h1').text
-    print(video_title)
-    views = driver.find_element_by_css_selector('#info-contents div#count').text
-    print(views)
+    print(getTitle())
+    print(getViews())
     print('=================================')
 
+def small_log():
+    time.sleep(1)
+    print(getTitle())
+    print('---------------------------------')
+
+def watch_video():
+    handle_ads()
+    time.sleep(getDuration())
+
+def watch_video_and_log():
+    handle_ads()
+    log_info()
+    time.sleep(getDuration())
+
+def watch_video_small_log():
+    handle_ads()
+    small_log()
+    time.sleep(getDuration())
+
 def video_cooldown():
-    # ass
     driver.get('https://www.youtube.com/watch?v=846cjX0ZTrk') # HI HIGH
     watch_video()
     driver.get('https://www.youtube.com/watch?v=XEOCbFJjRw0') # BUTTERFLY
@@ -70,57 +131,27 @@ def video_cooldown():
     driver.get('https://www.youtube.com/watch?v=FKlGHHhTOsQ') # SCREAM (DREAMCATCHER)
     watch_video()
 
-def to_sec(time):
-    return int(int(time[0])*60+int(time[1]))
+def look_for_next(videos):
+    for index, video in enumerate(videos):
+        title = video.text
+        if ('LOONA' in title or 'Dreamcatcher' in title):
+            return index
+    return False
 
-def watch_video_with_log():
-    # bot uses stupid logic to wait for ads and the whole video
-    duration= 0
-    duration2 = 1
-    while (duration != duration2 or duration2 < 60):
-        ActionChains(driver).move_to_element(driver.find_element_by_css_selector('ytd-player#ytd-player')).perform()
-        duration = driver.find_element_by_css_selector('span.ytp-time-duration').text.split(':')
-        duration = to_sec(duration)
-
-        if (duration > 150):
-            video_title = driver.find_element_by_css_selector('#container > h1').text
-            print('Watching: ' + video_title)
-            print('Duration:', duration)
-            time.sleep(duration)
-            break
-        elif (duration >= 15):
-            time.sleep(6)
-            driver.find_element_by_css_selector('div.ytp-ad-skip-button-text').click()
-            print('Skipped %s second ad' % duration)
+def video_cooldown2():
+    t = 0
+    while (t < 900):
+        side_videos = elements(other_videos)
+        next_vid = look_for_next(side_videos)
+        if (next_vid or next_vid == 0):
+            side_videos[next_vid].click()
         else:
-            time.sleep(duration)
-            print('Watched short ad (%s seconds)' % duration)
+            side_videos[3].click()
+        time.sleep(3)
+        watch_video_small_log()
+        d = getDuration()
+        t += d
 
-        ActionChains(driver).move_to_element(driver.find_element_by_css_selector('ytd-player#ytd-player')).perform()
-        duration2 = driver.find_element_by_css_selector('span.ytp-time-duration').text.split(':')
-        duration2 = to_sec(duration2)
-
-def watch_video():
-    # bot uses stupid logic to wait for ads and the whole video
-    duration= 0
-    duration2 = 1
-    while (duration != duration2 or duration2 < 60):
-        ActionChains(driver).move_to_element(driver.find_element_by_css_selector('ytd-player#ytd-player')).perform()
-        duration = driver.find_element_by_css_selector('span.ytp-time-duration').text.split(':')
-        duration = to_sec(duration)
-
-        if (duration > 150):
-            time.sleep(duration)
-            break
-        elif (duration >= 15):
-            time.sleep(6)
-            driver.find_element_by_css_selector('div.ytp-ad-skip-button-text').click()
-        else:
-            time.sleep(duration)
-
-        ActionChains(driver).move_to_element(driver.find_element_by_css_selector('ytd-player#ytd-player')).perform()
-        duration2 = driver.find_element_by_css_selector('span.ytp-time-duration').text.split(':')
-        duration2 = to_sec(duration2)
 
 # EXECUTION
 try:
@@ -132,19 +163,21 @@ try:
     mute.add_argument("--mute-audio")
     # bot will start and close a new window for each watch
     driver = webdriver.Chrome('.\\resources\\chromedriver', options=mute)
+    driver.maximize_window()
     driver.implicitly_wait(10)
     # bot goes to yt
     driver.get('https://www.youtube.com/')
-    
+    print('\n=================================')
     # main loop
     while (1):
-
-        watch_main_vid(video)
-        video_cooldown()
+        goto_vid(video)
+        disable_autoplay()
+        watch_video_and_log()
+        video_cooldown2()
 
 finally:
     # cleans the stuff
     if (driver != None) : driver.quit()
 
-# cleans the stuff twice just to be sure
+# cleans the stuff again just to be sure
 if (driver != None) : driver.quit()
